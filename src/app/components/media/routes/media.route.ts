d3f2207479241a5e573f9fc8f37a4d9afb196905
saurav gaur <saurav.21.gaur@gmail.com>
2021-05-11 00:19:51 +0530

    add extention to file upload in multer

--- src/app/components/media/routes/media.route.ts
+++ src/app/components/media/routes/media.route.ts
@@ -1,18 +1,51 @@
 import { Router } from "express";
 import {isAuthenticated,isAuthorized} from '../../../core/index'
 import multer from "multer";
+import * as shortid from 'shortid';
+import * as mime from 'mime-types';
+import {diskStorage,DiskStorageOptions} from "multer";
 import {MediaController } from "../controllers/media.controller";
 import { APP_PATHS } from "../../../core/constants/path.constants";
 const DIR = './uploads/'
 // import {RecordsCtrl} from "../controllers/records.controller"
 console.log("APP_PATHS-->",APP_PATHS)
-const uploadSnapshot = multer({dest:APP_PATHS.SNAPSHOT_DIR}).single('snapshot')
-// const uploadSnapshot = multer({dest:DIR}).single('snapshot')
-const uploadsupplementDoc = multer({dest:APP_PATHS.SUPPLIMENT_DIR}).single('supplementDoc')
-const uploadVerifcationDoc = multer({dest:APP_PATHS.VERFICATION_DIR}).single('verificationDoc')
+
+function fileNameHandler(req:any,file:any,cb:any){
+    let id = shortid.generate();
+        let ext = mime.extension(file.mimetype);
+        cb(null, `${id}.${ext}`);
+}
+
+const snapshotStorageOptions:DiskStorageOptions={
+    destination:(req,file,cb)=>{
+        cb(null, APP_PATHS.SNAPSHOT_DIR);
+    },
+    filename:fileNameHandler
+}
+
+const supplementStorageOptions:DiskStorageOptions={
+    destination:(req,file,cb)=>{
+        cb(null, APP_PATHS.SUPPLIMENT_DIR);
+    },
+    filename:fileNameHandler
+}
+
+const verificationStorageOptions:DiskStorageOptions={
+    destination:(req,file,cb)=>{
+        cb(null, APP_PATHS.VERFICATION_DIR);
+    },
+    filename:fileNameHandler
+}
+
+const snapshotStorage = diskStorage(snapshotStorageOptions)
+const supplementStorage = diskStorage(supplementStorageOptions)
+const verificationStorage = diskStorage(verificationStorageOptions)
+
+const uploadSnapshot = multer({storage:snapshotStorage}).single('snapshot')
+const uploadsupplementDoc = multer({storage:supplementStorage}).single('supplementDoc')
+const uploadVerifcationDoc = multer({storage:verificationStorage}).single('verificationDoc')
 
 let router=Router()
-console.log('saurav test---->')
 
 export function routesConfigInner():Router {
     let mediaController=new MediaController()
